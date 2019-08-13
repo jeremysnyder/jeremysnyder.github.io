4717936c88a9474b5d59a1eb68befec3a07d58bd
Jeremy Snyder <jeremy.snyder@gmail.com>
2019-08-12 21:17:06 -0400

    Initial T-APP

---
+++ tapp/precache-manifest.d286e7ab02be46a10d4a96303d19f9ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350c587b663ceef0ddaa89ee9280a3a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7fddeedd4fb339558d",
+    "url": "/static/css/main.022a8712.chunk.css"
+  },
+  {
+    "revision": "e8ad8d5aff9b55151361",
+    "url": "/static/js/2.b8236c1d.chunk.js"
+  },
+  {
+    "revision": "9b7fddeedd4fb339558d",
+    "url": "/static/js/main.8cb3c3d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
