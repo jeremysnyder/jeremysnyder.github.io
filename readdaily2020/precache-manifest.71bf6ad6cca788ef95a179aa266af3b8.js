24b6c8b42c9f43b6327530b05c5920c76ab1eea0
Jeremy Snyder <jeremy.snyder@gmail.com>
2019-12-26 08:39:05 -0500

    Added 2020 plan

---
+++ readdaily2020/precache-manifest.71bf6ad6cca788ef95a179aa266af3b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8d88dfb5d9e5ca3aecf4400a92c892",
+    "url": "/readdaily2/index.html"
+  },
+  {
+    "revision": "93f450ca22067e22f356",
+    "url": "/readdaily2/static/css/main.984ce417.chunk.css"
+  },
+  {
+    "revision": "46d42e15f976b082961a",
+    "url": "/readdaily2/static/js/2.8ee513d5.chunk.js"
+  },
+  {
+    "revision": "93f450ca22067e22f356",
+    "url": "/readdaily2/static/js/main.07802883.chunk.js"
+  },
+  {
+    "revision": "c54f7505b5a07fc69108",
+    "url": "/readdaily2/static/js/runtime-main.ae9567c6.js"
+  }
+]);
