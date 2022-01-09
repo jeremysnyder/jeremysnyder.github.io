fafbe80b6897ed15dd477a38df59290dca7b969a
Jeremy Snyder <jeremy.snyder@gmail.com>
2022-01-09 14:34:55 -0500

    Added small updates for 2022

---
+++ readdaily2022/precache-manifest.e035ecd2c2d028c6257d72fa77da016d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5041c25e2b38580d250198818a1d16b1",
+    "url": "/readdaily2021/index.html"
+  },
+  {
+    "revision": "41cc0e9cab38f4c742b7",
+    "url": "/readdaily2021/static/css/main.9e49055a.chunk.css"
+  },
+  {
+    "revision": "fc0a259b4c68b1ef19fa",
+    "url": "/readdaily2021/static/js/2.2f656b73.chunk.js"
+  },
+  {
+    "revision": "41cc0e9cab38f4c742b7",
+    "url": "/readdaily2021/static/js/main.4d32a1f4.chunk.js"
+  },
+  {
+    "revision": "48f14c4db5a3028365be",
+    "url": "/readdaily2021/static/js/runtime-main.55cf5804.js"
+  }
+]);
